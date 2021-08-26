e62ae91c311c453c607ed476086850f8fecb49fd
Smyja <akposlive59@gmail.com>
2021-08-26 20:27:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask,jsonify
+from marshmallow import fields, Schema, validates, ValidationError
 
 app = Flask(__name__)
+
+class TodoSchema(Schema):
+	id = fields.Int()
+	title = fields.Str()
+	description = fields.Str()
+	status = fields.Bool()
+    
 
 @app.route('/')
 def index():
@@ -21,6 +29,14 @@ tasks = [
     }
 ]
 
+@app.errorhandler(422)
+def validation_error(err):
+    """Handles 422 errors"""
+    messages = err.data.get('messages').get('json')
+    return jsonify(messages)
+    
+
+
 @app.route('/api/tasks', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks': tasks})
@@ -39,21 +55,22 @@ def create_task():
     return jsonify({'task': task}), 201
 
 @app.route('/api/tasks/<int:task_id>', methods=['PUT'])
+@validates('title')
 def update_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
     if not request.json:
-        abort(400)
+        raise ValidationError('Request is not json.')
     if 'title' in request.json and type(request.json['title']) != unicode:
         abort(400)
     if 'description' in request.json and type(request.json['description']) is not unicode:
         abort(400)
-    if 'status' in request.json and type(request.json['status']) is not bool:
+    if 'done' in request.json and type(request.json['done']) is not bool:
         abort(400)
     task[0]['title'] = request.json.get('title', task[0]['title'])
     task[0]['description'] = request.json.get('description', task[0]['description'])
-    task[0]['status'] = request.json.get('status', task[0]['status'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
     return jsonify({'task': task[0]})
 
 @app.route('/api/tasks/<int:task_id>', methods=['DELETE'])
