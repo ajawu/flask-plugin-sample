39c9e3c802ba86c20fe6350965ded40c3316d79b
Josiah Augustine <austinoski92@gmail.com>
2021-08-27 10:01:05 +0100

    Implement Activate Sidebar endpoints

--- src/plugin_sample/__init__.py
+++ src/plugin_sample/__init__.py
@@ -6,6 +6,10 @@ plugin_bp = Blueprint('plugin_sample', __name__, url_prefix='/', template_folder
 
 # Register Views
 plugin_bp.add_url_rule('/', view_func=views.home, methods=('GET', 'POST',))
+plugin_bp.add_url_rule('/sidebar', view_func=views.sidebar_api, methods=('GET',))
+plugin_bp.add_url_rule('/activate', view_func=views.activate_api, methods=('GET',))
+
+# Todo App Views
 plugin_bp.add_url_rule('/todo/create/', view_func=views.create_todo_api, methods=('POST',))
 plugin_bp.add_url_rule('/todo/<string:pk>', view_func=views.retrieve_todo_api, methods=('GET',))
 plugin_bp.add_url_rule('/todo/list/', view_func=views.list_todo_api, methods=('GET',))
